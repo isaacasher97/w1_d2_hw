992e7b99ae42d04cb182ff731d4e05db0cbbf031
isaacasher97 <isaac.asher97@gmail.com>
2023-05-02 02:35:34 -0400

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,24 @@
+//1. Create a loop that logs the numbers from 0-99 (ascending)
+// for(let i = 0; i <= 99; i++) {
+//     console.log(`I have logged this sentence ${i} times`)
+// }
+
+//2. Create a loop that logs the EVEN numbers from 98-0 (descending)
+// for(let i = 98; i >= 0; i-=2) {
+//     console.log(`I have only evenly logged this sentence ${i} times`)
+// }
+
+//3. Create a loop that logs the ODD numbers from 0-99 (ascending)
+// for(let i = 1; i <= 99; i+=2) {
+//     console.log(`I have oddly logged this sentence ${i} times`)
+// }
+
+//4. Create a loop that logs the numbers from 81-26 (descending)
+// for(let i = 81; i >= 26; i--) {
+//     console.log(`I have logged this sentence ${i}`)
+// }
+
+//5. Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+// for(let i = 3; i <= 90; i+=3) {
+//     console.log(`I have logged this sentence in multiples of three ${i} times`)
+// }
